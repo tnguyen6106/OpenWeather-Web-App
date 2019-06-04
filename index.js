d2097b8bc3b5a47b65ceb16cf919645293492619
mtxrg <mitxi@pdx.edu>
2019-06-04 11:39:38 -0700

    Started js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //JS code
+
+const key = "b72d0821934296a82580c9a8b315a076"; //API key
+
+city = document.getElementById("search-text");
+seach = document.getElementById("search-button");
+
+search.addEventListener("click", getWeather);
+
+function getWeather(){
+  request = "https://api.openweathermap.org/data/2.5/weather?q=" + search.value + "&appid=" + key;
+  httpsRequestAsync(search, response);
+}
+
+function response(response){
+  //parse json 
+}
