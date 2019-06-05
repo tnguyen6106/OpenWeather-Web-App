572d264d5459d1736633a87bfd6e2d4a4c5afd6d
mtxrg <mitxi@pdx.edu>
2019-06-04 18:18:32 -0700

    Updated css, js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ temp = document.getElementById("temp");
 maxmin = document.getElementById("maxmin");
 cloudiness = document.getElementById("cloudiness");
 icon = document.getElementById("icon");
+cityname = document.getElementById("cityname");
 wind = document.getElementById("wind");
 description = document.getElementById("description");
 
@@ -25,15 +26,14 @@ function getWeather(){
 }
 
 function responses(response){
-  let jsonObject = JSON.parse(response);
-  //temp, snow, cloudiness, wind speed, rain volume, humidity
-  city.innerHTML = jsonObject.name;
+  let jsonObject = JSON.parse(response);//temp, snow, cloudiness, wind speed, rain volume, humidity
+  cityname.innerHTML = jsonObject.name;
   icon.src = "http://openweathermap.org/img/w/" + jsonObject.weather[0].icon + ".png";
-  temp.innerHTML = parseInt(jsonObject.main.temp * (9/5) - 459.67) + "°";
-  humidity.innerHTML = jsonObject.main.humidity + "%";
-  maxmin.innerHTML = "Min: " + parseInt(jsonObject.main.temp_min * (9/5) - 459.67) + " Max: " + parseInt(jsonObject.main.temp_max * (9/5) - 459.67);
-  cloudiness.innerHTML = jsonObject.clouds.all + "%";
-  wind.innerHTML = jsonObject.wind.speed + " meter/sec";
+  temp.innerHTML = "Temp: " + parseInt(jsonObject.main.temp * (9/5) - 459.67) + "°";
+  humidity.innerHTML = "Humidity: " + jsonObject.main.humidity + "%";
+  maxmin.innerHTML = "Min/Max: " + parseInt(jsonObject.main.temp_min * (9/5) - 459.67) + "°/" + parseInt(jsonObject.main.temp_max * (9/5) - 459.67) + "°";
+  cloudiness.innerHTML = "Cloudiness: " + jsonObject.clouds.all + "%";
+  wind.innerHTML = "Wind: " + jsonObject.wind.speed + " meter/sec";
   description.innerHTML = jsonObject.weather[0].description;
 
 
